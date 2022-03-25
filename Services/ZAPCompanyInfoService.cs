f6db9614026ad13b3e7d7fc50f95cedcc98d1b8b
Jory Smith <46983170+JorySmith@users.noreply.github.com>
2022-03-25 11:20:55 -0500

    Updated ZAPCompanyInfoService

--- Services/ZAPCompanyInfoService.cs
+++ Services/ZAPCompanyInfoService.cs
@@ -20,7 +20,7 @@ namespace ZapperBugTracker.Services
         // Get all members for a companyId
         public async Task<List<ZUser>> GetAllMembersAsync(int companyId)
         {
-            // Instantiate a new blank list of ZUsers to store user query results
+            // Instantiate a new blank list of ZUsers to store and return the user query results
             List<ZUser> result = new();
 
             // Store async list of users that match input companyId
@@ -32,7 +32,26 @@ namespace ZapperBugTracker.Services
         // Get all projects for a companyId
         public async Task<List<Project>> GetAllProjectsAsync(int companyId)
         {
-            throw new NotImplementedException();
+            // Instantiate a blank list of type project to store and return project query results
+            List<Project> result = new();
+
+            // Await DB query of projects that match input companyId, store in a list async
+            // Eager load/Include navigation properties for each project:
+            // Members, Tickets (then include their comments/status/pri), and ProjectPri 
+            result = await _context.Projects.Where(p => p.CompanyId == companyId)
+                                            .Include(p => p.Members)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Comments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.ProjectPriority)                                            
+                                            .ToListAsync();
+
+            return result;
         }
 
         // Get all tickets for a companyId
